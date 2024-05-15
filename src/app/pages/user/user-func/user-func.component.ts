f2d82f4eeb38e379459a26742dc97e19d6a592a5
Branko Pualić-Radujko <pualicbranko@gmail.com>
2024-05-15 18:05:58 +0200

    Added: Exercises component on my profile and set up default active tab

--- src/app/pages/user/user-func/user-func.component.ts
+++ src/app/pages/user/user-func/user-func.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ViewChild,
+} from '@angular/core';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-user-func',
   templateUrl: './user-func.component.html',
-  styleUrl: './user-func.component.scss'
+  styleUrl: './user-func.component.scss',
 })
-export class UserFuncComponent {
+export class UserFuncComponent implements AfterViewInit {
+  @ViewChild(TabsetComponent)
+  tabset?: TabsetComponent;
 
+  constructor(private cdr: ChangeDetectorRef) {}
+  ngAfterViewInit(): void {
+    if (this.tabset) {
+      this.tabset.tabs[2].active = true;
+      this.cdr.detectChanges();
+    }
+  }
 }
